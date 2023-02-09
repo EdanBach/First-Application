1856dc140563a81018391c078ae57cc795e078b8
quocbach <bachtrungquoc2910>
2023-02-09 23:20:43 +0700

    Initial commit

---
+++ app/src/main/java/hcmute/edu/first_application/MainActivity.java
@@ -0,0 +1,15 @@
+package hcmute.edu.first_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
